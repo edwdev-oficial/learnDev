4d30522c7911bb95cbbd3b617e5dfa78ca5a510b
Eduardo <edjdevel@gmail.com>
2022-02-08 21:25:22 -0300

    criação do cor da aplicação

---
+++ api_learnDev/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyparser = require('../node_modules/body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 8081;
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+    res.send({ mensagem: 'app rodando ok!' });
+});
+
+
+app.listen(port, () => {
+    console.log(`app loading in url http://localhost:${port}`)
+});
+
