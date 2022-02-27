6eec7f92c8700ffdfc66576bee9d87642bab73db
Eduardo <edjdevel@gmail.com>
2022-02-27 12:58:10 -0300

    temaController.js in app changed

--- app_learnDev/controller/temaController.js
+++ app_learnDev/controller/temaController.js
@@ -1,6 +1,9 @@
 import { Tema } from '../model/tema.js';
 import { TopicoController } from './topicoController.js';
 
+const header = document.querySelector('header');
+const tema = header.querySelector('h1');
+
 export class TemaController {
 
     async getTemas() {
@@ -12,6 +15,8 @@ export class TemaController {
     };
 
     printTemas(temas) {
+
+        metodo.removeTemas();
 
         let index = 0;
         let cor;
@@ -48,6 +53,7 @@ export class TemaController {
                     TopicoController.getTopicos(element._id);
                 }else{
                     header.textContent = 'LearnDev';
+                    header.classList.remove(...header.classList)
                     TopicoController.removeTopicos();
                 };
 
@@ -57,7 +63,82 @@ export class TemaController {
 
     };
 
+    startAddTema() {
+        const btnAdd = document.querySelector('.altera-menu-mais');
+        btnAdd.addEventListener('click', () => {
+            if (tema.classList.length == 0) {
+                metodo.addTema(tema);
+            }
+        });
+
+    }
+
+    startDeleteTema() {
+        
+        const btnDelete = document.querySelector('.altera-menu-menos')
+
+        btnDelete.addEventListener('click', (target) => {
+
+            if(tema.classList.length !== 0) {
+                metodo.deleteTema(tema.classList[0]);
+            };
+
+        });
+        
+    }
+
+    async addTema(tema) {
+        const data = {
+            nome: prompt('Informe o tema a ser criado')
+        }
+
+        const temas = await Tema.addTema(data)
+
+        metodo.printTemas(temas)
+    
+    };
+
+    async deleteTema(idTema) {
+
+        const btnTema = document.getElementById(idTema)
+        
+        if(prompt('Confirme o nome do tema a ser excluido') == btnTema.innerHTML) {
+            
+            const data = {
+                _id: idTema
+            };
+
+            tema.innerHTML = 'LearnDev';
+            tema.classList.remove(...tema.classList);
+            console.log(tema)
+
+            const temas = await Tema.deteleTema(data);
+            
+            metodo.printTemas(temas);
+
+        }else {
+            alert('Nome divergente')
+        };
+
+    };
+
+    removeTemas() {
+
+        const temas = document.querySelectorAll('.ul-tema');
+
+        if(temas.length > 0) {
+
+            for(let i = temas.length -1; i>= 0; i--) {
+                temas[i].remove();
+            };
+
+        };
+
+    };
+
 };
 
 const metodo = new TemaController();
 metodo.getTemas();
+metodo.startAddTema();
+metodo.startDeleteTema();
